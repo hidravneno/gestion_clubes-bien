6fced43a9613e288445c9c4477507be07674ab18
JahelCesun <jahel.andrademurillo@cesunbc.edu.mx>
2025-04-04 00:37:33 -0700

    Foros fixes

--- foros/forms.py
+++ foros/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Foro
+from .models import Foro, Club
 
 
 class CrearForoForm(forms.ModelForm):
@@ -7,3 +7,7 @@ class CrearForoForm(forms.ModelForm):
         model = Foro
         fields = ['club', 'titulo', 'contenido', 'creado_por']
 
+    def __init__(self, *args, **kwargs):
+        super(CrearForoForm, self).__init__(*args, **kwargs)
+        self.fields['club'].queryset = Club.objects.all().order_by('nombre') 
+        self.fields['club'].label_from_instance = lambda obj: obj.nombre
