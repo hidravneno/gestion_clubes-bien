149b72c23e49125cb37f6dd7b503cea02a2c5171
hidravneno <aramburoeduardo972@gmail.com>
2025-04-07 09:55:03 -0700

    Tu mensaje de commit aquí

--- clubes/urls.py
+++ clubes/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Cambiado de 'index' a 'home'
+    path('', views.home, name='home'),  # Página principal
     path('crear/', views.crear_club, name='crear_club'),  # Crear un nuevo club
     path('lista/', views.lista_clubes, name='lista_clubes'),  # Lista de clubes
     path('<int:club_id>/editar/', views.editar_club, name='editar_club'),  # Editar club
+    path('<int:club_id>/', views.detalle_club, name='detalle_club'),  # **Nueva ruta para detalle_club**
 
     # Rutas para reuniones
     path('<int:club_id>/reuniones/', views.lista_reuniones, name='lista_reuniones'),  # Lista de reuniones
